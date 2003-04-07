29bb5347bea0f6539b2f35940d4cea63e7087232
Bill Allombert <ballombe@debian.org>
2003-04-07 16:14:23 +0000

    Import from menu-2.1.7-3.

---
+++ autogen.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+#This very stupid shell script is Public Domain.
+
+errorconf="returned an error code or did not exists at all. \
+You need autoconf 2.13 and automake >= 1.4. Sorry!"
+
+aclocal || { echo "aclocal $errorconf"; exit 1; }
+autoconf || { echo "autoconf $errorconf"; exit 1; }
+autoheader || { echo "autoheader $errorconf"; exit 1; }
+automake -a || { echo "automake $errorconf"; exit 1; }
+./configure --enable-maintainer-mode $*
