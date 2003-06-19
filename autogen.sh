79f930f3024a88d2b65e7a71b882c8e5750dfbfa
Morten Brix Pedersen <morten@wtf.dk>
2003-06-19 14:58:08 +0000

    My previous changes to configure.ac actually puts a requirement to autoconf 2.50 (not 2.13 which was stated in autogen.sh). Fixed.

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 #This very stupid shell script is Public Domain.
 
 errorconf="returned an error code or did not exists at all. \
-You need autoconf 2.13 and automake >= 1.4. Sorry!"
+You need autoconf >= 2.50 and automake >= 1.4. Sorry!"
 
 aclocal || { echo "aclocal $errorconf"; exit 1; }
 autoconf || { echo "autoconf $errorconf"; exit 1; }
